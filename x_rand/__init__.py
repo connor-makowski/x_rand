2f4b73bd2962726431eb0fb461f7643df940bd75
Connor <connorman528@gmail.com>
2020-01-13 13:01:10 -0500

    Remove unnecessayr comments

--- x_rand/__init__.py
+++ x_rand/__init__.py
@@ -1,7 +1,5 @@
 import sys
-# For python 3
 if sys.version_info[0] == 3:
     from x_rand.x_rand import x_rand, x_rand_admin
-# For python 2
 elif sys.version_info[0] < 3:
     from x_rand import x_rand, x_rand_admin
