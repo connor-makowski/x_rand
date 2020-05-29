5dabe0c22340cb532c4eacdf754b8991c6508793
Connor Makowski <connorman528@gmail.com>
2020-05-29 07:00:07 -0400

    Update docs

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from x_rand import *
--- update_documentation.sh
+++ update_documentation.sh
@@ -1,3 +1,4 @@
-pdoc . --html --overwrite --html-dir docs
+# Must be installed locally to work `pip install -e .`
+pdoc --html --html-dir docs --overwrite x_rand
 mv ./docs/x_rand/* ./docs
 rm -r ./docs/x_rand
