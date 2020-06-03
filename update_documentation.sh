cc66935bb0f2442de34de271fd07a8161da153bb
Connor Makowski <connorman528@gmail.com>
2020-06-03 07:07:33 -0400

    Bump to v0.6 to update class structures and docs

--- update_documentation.sh
+++ update_documentation.sh
@@ -1,4 +1,5 @@
 # Must be installed locally to work `pip install -e .`
+# Must also have pydoc3 installed locally `pip install -r dev_requirements.txt`
 pdoc --html --force -o docs x_rand/
 mv ./docs/x_rand/* ./docs
 rm -r ./docs/x_rand
