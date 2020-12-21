73a14b1228cc544011f1c0e43267e1afd66aeaf8
Connor Makowski <connorman528@gmail.com>
2020-12-21 14:34:57 -0500

    Update Docs

--- update_documentation.sh
+++ update_documentation.sh
@@ -1,5 +1,5 @@
 # Must be installed locally to work `pip install -e .`
 # Must also have pydoc3 installed locally `pip install -r dev_requirements.txt`
-pdoc --html --force -o docs x_rand/
-mv ./docs/x_rand/* ./docs
-rm -r ./docs/x_rand
+pdoc --html --force -o docs x_rand/ x_rand2/
+mv ./docs/x_rand2/* ./docs
+# rm -r ./docs/x_rand
